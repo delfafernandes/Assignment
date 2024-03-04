ece72821e3b961381551c763da8844f6b6bd06d9
Delfa Fernandes <fernandesdelfa1234@gmail.com>
2024-03-04 10:58:16 +0530

    1st

--- Isa2/src/assign__/Integer.java
+++ Isa2/src/assign__/Integer.java
@@ -1,20 +1,16 @@
 package assign__;
 
-import java.util.Scanner;
-
 public class Integer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner scan =new Scanner(System.in);
-		System.out.println("Enter first number: ");
-		int Num1=scan.nextInt();
-		System.out.println("Enter second number: ");
-		int Num2=scan.nextInt();
-		int Add= Num1+Num2;
-		System.out.println(Num1 +" + "+ Num2 +" = "+ Add);
+		int Num1=15;
+		int Num2=15;
+		int add= Num1+Num2;
+		System.out.println("The sum of to integer is"+ add);
+	}
 		
 	}
 
-}
+
